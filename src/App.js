372ba2b35697829dad16bf535097e86d798537a8
Emily Venturino <emily.venturino@outlook.com>
2022-09-01 19:36:53 -0500

    adding button toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Choices from "./components/Choices/choices";
 
 function App() {
+  const [compIsShown, setCompIsShown] = useState(false);
+  // const [isShow, setIsShow] = useState(false);
+
+  // useEffect(() => {
+  //   setIsShow(isBoolean);
+  // }, [isBoolean, setIsShow]);
+
+  // const shufler = () => {
+  //   setIsShow(false);
+  // };
+
+  // if (!isShow) {
+  //   return null;
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-
-       This is my base app
-      </header>
-      <div className="quiz-box"> This is a box </div>
+      <header className="App-header">This is my base app</header>
+      <div className="quiz-box">
+        <button className="start-button" onClick={() => setCompIsShown(true)}>
+          Start
+        </button>
+        {compIsShown && <Choices />}
+      </div>
     </div>
   );
 }
---
+++ src/components/Choices/choices.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const Choices = () => {
+  return (
+    <div>
+      This is going to be a question......?
+      <div className="answers-group">
+        <button className="answers">answer 1</button>
+        <button className="answers">answer 2</button>
+        <button className="answers">answer 3</button>
+        <button className="answers">answer 4</button>
+      </div>
+    </div>
+  );
+};
+
+export default Choices;
